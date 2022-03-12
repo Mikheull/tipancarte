471c62dad7e2e0a644de59903f2c69ac86cadf87
Mikheull <Mikhael.bailly1@orange.fr>
2022-03-12 18:14:43 +0100

    fix: gtm

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,41 +5,53 @@ import Header from './Header'
 function Layout({ actual, title, description, children }) {
 
     return (
-        <>
-            <noscript>
-                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-542RP25" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
-            </noscript>
+        <div>
+            <Head>
+                <title> {title ? `TiPancarte - ${title} ` : 'TiPancarte '} </title>
+                <meta name="description" content={description ? description : "Découvrez les pancartes personnalisable et commandez la votre en utilisant notre outil de configuration. Les pancartes style martinique sont faites 100% manuellement et livrée dans toute la France"} />
+                <link rel="icon" href="/favicon.ico" />
+
+                {/* Axeptio integration */}
+                <script
+                    dangerouslySetInnerHTML={{
+                    __html: `
+                        window.axeptioSettings = {
+                            clientId: "622cca37447787b78624eb67",
+                        };
+                        
+                        (function(d, s) {
+                            var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+                            e.async = true; e.src = "//static.axept.io/sdk.js";
+                            t.parentNode.insertBefore(e, t);
+                        })(document, "script");
+                    `,
+                    }}
+                />
+                {/* Google Analytics integration */}
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-QC95VHZFC7`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-QC95VHZFC7', {
+                        page_path: window.location.pathname,
+                    });
+                `,
+                    }}
+                />
+            </Head>
+
+            <Header actual={actual}/>
             <div>
-                <Head>
-                    <title> {title ? `TiPancarte - ${title} ` : 'TiPancarte '} </title>
-                    <meta name="description" content={description ? description : "Découvrez les pancartes personnalisable et commandez la votre en utilisant notre outil de configuration. Les pancartes style martinique sont faites 100% manuellement et livrée dans toute la France"} />
-                    <link rel="icon" href="/favicon.ico" />
-
-                    {/* Google Tag Manager integration */}
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=G-QC95VHZFC7`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                        __html: `
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-542RP25');
-                        `,
-                        }}
-                    />
-                </Head>
-
-                <Header actual={actual}/>
-                <div>
-                    {children}
-                </div>
-                <Footer />
+                {children}
             </div>
-        </>
+            <Footer />
+        </div>
     )
 }
 
