e55e22ee34b9723f64c2ee0ad67a2a89fe9ddfc7
Mikheull <Mikhael.bailly1@orange.fr>
2022-03-12 18:33:08 +0100

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
       <Html lang="fr">
         <Head>
             <meta charset="utf-8" />
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+            <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8"/>
             {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/> */}
 
             <meta name="language" content="fr"/>
