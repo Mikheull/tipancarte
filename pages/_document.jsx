2ebc4f19f103087c88e8ccab554e02d9600f0016
Mikheull <Mikhael.bailly1@orange.fr>
2022-03-13 19:32:51 +0100

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -27,48 +27,20 @@ export default class MyDocument extends Document {
   }
 }
 
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with static-site generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
+// MyDocument.getInitialProps = async (ctx) => {
+//   const originalRenderPage = ctx.renderPage;
 
-  const originalRenderPage = ctx.renderPage;
+//   ctx.renderPage = () =>
+//     originalRenderPage({
+//       // eslint-disable-next-line react/display-name
+//       enhanceApp: (App) => (props) => <App {...props} />,
+//     });
 
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      // eslint-disable-next-line react/display-name
-      enhanceApp: (App) => (props) => <App {...props} />,
-    });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    // This is important. It prevents emotion to render invalid HTML.
-    // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+//     const initialProps = await Document.getInitialProps(ctx);
   
-    return {
-      ...initialProps,
-      // Styles fragment is rendered after the app and page rendering finish.
-      styles: [...React.Children.toArray(initialProps.styles)],
-    };
-  };
-  
+//     return {
+//       ...initialProps,
+//       // Styles fragment is rendered after the app and page rendering finish.
+//       styles: [...React.Children.toArray(initialProps.styles)],
+//     };
+//   };
